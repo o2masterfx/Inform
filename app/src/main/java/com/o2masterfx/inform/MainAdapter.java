87802d16c3da5c2791e395bcd6d8805603757fb2
Rizky92 <myquinn489@gmail.com>
2020-04-20 11:22:29 +0800

    Testing 3

--- app/src/main/java/com/o2masterfx/inform/MainAdapter.java
+++ app/src/main/java/com/o2masterfx/inform/MainAdapter.java
@@ -1,4 +1,32 @@
 package com.o2masterfx.inform;
 
-public class MainAdapter {
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MainAdapter extends RecyclerView.Adapter<MainAdapter.ViewHolder> {
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(@NonNull View view) {
+            super(view);
+        }
+    }
 }
